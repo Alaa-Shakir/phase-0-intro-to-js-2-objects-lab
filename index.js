5c46a1a6bec9e72834cb625fd5031a4b7e3e7928
Alaa Shakir <alaa.2000.shakr@gmail.com>
2024-01-16 23:43:12 +0300

    completed lab tasks

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution in this file!
+const employee = {
+  name: "Alaa",
+  streetAddress: "hasan harayn",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newObj = { ...employee };
+
+  newObj["streetAddress"] = "11 Broadway";
+
+  return newObj;
+}
+
+beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  });
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+
+    employee["streetAddress"] = "12 Broadway";
+
+    return employee;
+  }
+
+  function deleteFromEmployeeByKey(employee, key){ 
+    const newObj = {...employee};
+    delete newObj.name;
+    return newObj;
+  }
+
+  beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  });
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key){ 
+    delete employee.name;
+    return employee;
+  }
